5359f20dac8465c4b1edd7f50705a177e89bc8e4
Яков Серебренников <serebrennikov@rbsoft.ru>
2023-05-27 16:48:49 +0800

    Знакомство с jsx компонентом.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const name = "Yakov";
+const foo = (num1, num2) => num1 + num2;
+const App = () => {
+  return(
+    <div>
+      <h1>Имя - {name}, функция сложения (10 и 20): {foo(10, 20)}</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello world!</h1>
+    <App />
   </React.StrictMode>
 );
 
