52ed984910a6bba470fed4e1f5f74db24f1536c3
Яков Серебренников <serebrennikov@rbsoft.ru>
2023-05-31 22:37:18 +0800

    Добавил аватарам разные фото и описания.

--- src/data/data.js
+++ src/data/data.js
@@ -3,17 +3,17 @@ export const bloggers = [
       id: "1",
       name:"Сергей Сергеевич",
       avatar: "https://i.vimeocdn.com/portrait/60837690_150x150",
-      bio:"vnlsdkjfvnlsdfvnlsjfvn lkjfdvnlsfvnsldfjvn sljfkvnlsdfjvnlsfvnlsfj."
+      bio:"Характер: Вспыльчивый, Злой, Грустный"
   },
     {
       id: "2",
       name:"Петр Петров",
-      avatar: "https://i.vimeocdn.com/portrait/60837690_150x150",
-      bio:"vnlsdkjfvnlsdfvnlsjfvn lkjfdvnlsfvnsldfjvn."
+      avatar: "https://klike.net/uploads/posts/2023-01/1674365337_3-31.jpg",
+      bio:"Характер: Умереный, Добрый, Весёлый"
   },
     {
       id: "3",
       name:"Петр Петров",
-      avatar: "https://i.vimeocdn.com/portrait/60837690_150x150",
-      bio:"vnlsdkjfvnlsdfvnlsjfvn lkjfdvnlsfvnsldfjvn."
+      avatar: "https://e7.pngegg.com/pngimages/162/125/png-clipart-internet-of-things-information-digital-marketing-others-miscellaneous-face.png",
+      bio:"Характер: Умереный, Злой, Грустный"
   }];
